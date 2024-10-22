7132cf993120f77844fa6d866db958647d543c60
agilimam <agilimamalhafizi04@gmail.com>
2024-10-22 15:29:47 +0700

    Membuat Router

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import MobilRoute from "./Routes/Mobil.js";
+
+const app = express();
+const PORT = 8000;
+
+app.use(bodyParser.json());
+
+app.use("/Mobil", MobilRoute)
+app.get("/", (req, res) => {
+    console.log(['GET ROUTE']);
+    res.send("Selamat Pagiiii");
+});
+
+
+app.listen(PORT, () => console.log(
+    `Server berjalan di port: http://localhost:${PORT}`
+));
